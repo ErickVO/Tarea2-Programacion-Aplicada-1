2342c66bb4c1cb27f4d23d437f7766221b2f7d32
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-01-25 17:50:37 -0400

    Ejercicios de la Unidad 5,6,7,8

---
+++ Tarea2/DosCadenas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace DosCadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Cad1, Cad2;
+
+            Console.WriteLine("Introduzca la primera cadena:");
+            Cad1 = Console.ReadLine();
+
+            Console.WriteLine("Introduzca la segunda cadena:");
+            Cad2 = Console.ReadLine();
+
+             Console.Clear();
+
+            if (Cad1.CompareTo(Cad2) < 0)
+            {
+                Console.WriteLine("El orden es:\n{0}",Cad1);
+                Console.WriteLine("{0}", Cad2);
+            }
+            else
+            {
+                Console.WriteLine("El orden es: \n{0}", Cad2);
+                Console.WriteLine("{0}", Cad1);
+                
+            }
+        }
+
+      
+    }
+}

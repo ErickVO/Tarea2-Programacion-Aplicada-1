150f352acb1cc28560b6e0992ad784366cf1c44f
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-02-01 17:12:30 -0400

    Add files via upload

---
+++ Tarea3/Ejercicio9_3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace Ejercicio9_3
+{
+    class Program
+    {
+        public struct Dueno
+        {
+            public String Nombre;
+            public String Direccion;
+            public String Telefono;
+
+            public Dueno(String Nombre1, String Direccion1, String Telefono1)
+            {
+                Nombre = Nombre1;
+                Direccion = Direccion1;
+                Telefono = Telefono1;
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sd = new StringBuilder();
+                sd.AppendFormat("Nombre del Dueño: {0}, Telefono del Dueño: {2}, Dirección del Dueño: {1}", Nombre, Direccion, Telefono);
+                return sd.ToString();
+            }
+        }
+
+        public struct Mascota
+        {
+            public String Nombre;
+            public String Raza;
+            public Dueno Jefe;
+
+            public Mascota(String NombreMascota, String Raza2, String NombreD, String DireccionD, String TelefonoD)
+            {
+                Nombre = NombreMascota;
+                Raza = Raza2;
+                Jefe = new Dueno(NombreD, DireccionD, TelefonoD);
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sd = new StringBuilder();
+                sd.AppendFormat("Nombre de la Mascota: {0}, Raza de la Mascota: {1}", Nombre, Raza);
+                sd.Append(Jefe.ToString());
+                return sd.ToString();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tarea3/Ejercicio9_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio9_4
+{
+    class Program
+    {
+        enum Neumaticos {Diagonal=1,Radial,Verano,Invierno,AllSeason,Asimetrico,Direccionales,Tubuless,
+        PerfilBajo,ReCauchutados,RunFlat,Ecologicos}
+        static void Main(string[] args)
+        {
+            Neumaticos neumaticos;
+            neumaticos = Neumaticos.Diagonal;
+            Console.WriteLine("Los neumaticos son: {0}", neumaticos); 
+        }
+    }
+}

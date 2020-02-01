399c165dcc9901063c6435380ad23e3734db12e3
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-02-01 17:42:35 -0400

    Ejercicios  9,10,12

---
+++ Tarea3/Ejercicios_12_todos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicios_12_todos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num;
+
+            Console.Write("Introduzca un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El factorial de {0} es {1}", num, factorial(num));
+
+
+        }
+
+
+        public static int factorial(int num)
+        {
+            int res = 1;
+
+            for (int i = num; i > 1; i--)
+            {
+                res *= i;
+            }
+
+            return res;
+        }
+    }
+}
